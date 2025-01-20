eb2d435cf0536e4b21ae50bd0150c3691203f7f2
Yuliia <123005661+Yuliia-Diachenko@users.noreply.github.com>
2025-01-20 22:14:45 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,7 +7,6 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 const PORT = Number(env('PORT', '3000'));
-const cors = require('cors');
 const corsOptions = { origin: '*',
 methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
 allowedHeaders: ['Content-Type', 'Authorization'] };
@@ -16,7 +15,7 @@ export default async function startServer() {
 
   const app = express();
   app.use(express.json());
-  app.use(cors(corsOptions));
+  app.use(cors());
   app.use(
     pino({
       transport: {
