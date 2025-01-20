1c56e14ab4b25db806ab0cb7d09bd2478a3dbbc2
Yuliia <123005661+Yuliia-Diachenko@users.noreply.github.com>
2025-01-20 22:39:57 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ export default async function startServer() {
 
   const app = express();
   app.use(express.json());
-  app.use(cors());
+  app.use(cors({ origin: 'http://localhost:5173' }));
   app.use(
     pino({
       transport: {
